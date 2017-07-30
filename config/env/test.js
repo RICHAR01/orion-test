78bcb1426cd79987570dba69d9fdd3d9d8fc81d7
gdragon <gdragon@gdragons-MacBook-Pro.local>
2017-07-30 14:30:52 -0700

    test

--- config/env/test.js
+++ config/env/test.js
@@ -1,5 +1,9 @@
 export default {
   session: 'secret-boilerplate-token',
   token: 'secret-jwt-token',
-  database: 'mongodb://localhost:27017/koa2-boilerplate-test'
+  database: 'mongodb://localhost:27017/koa2-boilerplate-test',
+  cors: {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }
 }
