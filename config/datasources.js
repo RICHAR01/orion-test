e1a2e0be809a7950b5defe7ddf4f4eb0035faa21
userName <username@domain.com>
2024-10-09 16:25:47 -0700

    Get MONGO_URL from ENV variables

--- config/datasources.js
+++ config/datasources.js
@@ -1,4 +1,14 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
+const MONGO_URL = process.env.MONGO_URL || null;
 const config = require(`./datasources/${env}`);
+
+// Set env variables for DB connection
+if (MONGO_URL) {
+  console.log(
+    `Env variable MONGO_URL available, overring db connection name 'mongodb' with new connection url.`
+  );
+  const mongoDBIndex = config.findIndex((x) => x.name === "mongodb");
+  config[mongoDBIndex].url = MONGO_URL;
+}
 
 export default config;
